74d395ecce29a6d8b862b27367066b7285b8eb5a
Noah <m.n.vanos@students.uu.nl>
2023-05-30 14:31:33 +0200

    JS-14

---
+++ test/rules/JS-14.js
@@ -0,0 +1,11 @@
+const person = {
+    firstName: 'testFirstName',
+    lastName: 'testLastName'
+};
+
+const person2 = new Object();
+
+function Person(fname, lname) {
+    this.firstName = fname;
+    this.lastName = lname;
+}
