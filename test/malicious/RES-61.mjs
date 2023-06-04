1bb50b5e7acfd78eb76dafc7592a5de9c1eaa6f9
Noah <m.n.vanos@students.uu.nl>
2023-06-04 17:08:22 +0200

    RES-61

---
+++ test/malicious/RES-61.mjs
@@ -0,0 +1,17 @@
+import express from 'express';
+import multer from 'multer';
+const upload = multer({ dest: 'uploads/' });
+import path from 'path';
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("index.html"));
+});
+
+app.post("/upload", upload.single('fileToUpload'), (req, res) => {
+    console.log(req.file);
+    res.send("upload complete");
+});
+
+app.listen(80);
