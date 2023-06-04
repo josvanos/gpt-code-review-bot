399f99b8fd0109266d7426b6d4304a4600db5eac
Noah <m.n.vanos@students.uu.nl>
2023-06-04 20:19:10 +0200

    RES-68

---
+++ test/malicious/RES-68.py
@@ -0,0 +1,7 @@
+from flask import request, render_template_string
+
+@app.route('/hello')
+def hello():
+    username = request.args.get('username')
+    template = "<p>Hello {{ name }}</p>"
+    return render_template_string(template, name=username) # Compliant
