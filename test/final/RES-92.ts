7748bb4c75e099f8eeeb966192c3d83aa274ec0e
Noah <m.n.vanos@students.uu.nl>
2023-06-06 10:14:23 +0200

    RES-92

---
+++ test/final/RES-92.ts
@@ -0,0 +1,18 @@
+
+const BASE_SALARY = 40000
+
+interface Employee {
+    name: string;
+    role: string;
+    gender: "female" | "male";
+    yearsAtCompany: number;
+    skills: string[];
+}
+
+function calculateSalary(employee: Employee) {
+
+    if (employee.role == "janitor") return BASE_SALARY - 10000;
+    if (employee.role == "manager") return BASE_SALARY + 15000;
+
+    return BASE_SALARY;
+}
